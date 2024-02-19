f5d53a13d3587bae7fd32fdb4a49c0cb77a6a399
Halla Raafat <halla.raafat2000@gmail.com>
2024-02-19 16:43:48 +0200

    Project for all arm tasks

---
+++ ARM_STM/include/HAL/LED_Cfg.h
@@ -0,0 +1,15 @@
+/*
+ * LED_Cfg.h
+ *
+ *  Created on: Feb 19, 2024
+ *      Author: Lenovo
+ */
+
+#ifndef LED_CFG_H_
+#define LED_CFG_H_
+
+
+
+
+
+#endif /* LED_CFG_H_ */
---
+++ ARM_STM/src/HAL/LED_Cfg.c
@@ -0,0 +1,10 @@
+/*
+ * LED_Cfg.c
+ *
+ *  Created on: Feb 19, 2024
+ *      Author: Lenovo
+ */
+
+
+
+
