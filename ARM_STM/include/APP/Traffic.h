2458f1c5ad0a15949be7ef481adf377e3d01478b
Halla Raafat <halla.raafat2000@gmail.com>
2024-03-18 05:02:14 +0200

    Traffic state machine

---
+++ ARM_STM/include/APP/Traffic.h
@@ -0,0 +1,15 @@
+/*
+ * Traffic.h
+ *
+ *  Created on: Mar 18, 2024
+ *      Author: Halla Raafat
+ */
+
+#ifndef TRAFFIC_H_
+#define TRAFFIC_H_
+
+void Traffic_Init();
+
+void Traffic_Runnable();
+
+#endif /* TRAFFIC_H_ */
